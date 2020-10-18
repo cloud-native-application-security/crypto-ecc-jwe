b5ab0ac69ac54b401f6a73290140b6eef9e2eea7
Adib Saikali <asaikali@gmail.com>
2020-10-18 02:43:26 -0400

    WIP

--- util/src/main/java/com/example/util/KeyExchange.java
+++ util/src/main/java/com/example/util/KeyExchange.java
@@ -1,54 +1,51 @@
 package com.example.util;
 
 import com.google.crypto.tink.subtle.Hkdf;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.Curve;
+import com.nimbusds.jose.jwk.ECKey;
+import com.nimbusds.jose.jwk.JWK;
 import java.security.GeneralSecurityException;
-import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
+import java.security.interfaces.ECPublicKey;
+import java.text.ParseException;
 import javax.crypto.KeyAgreement;
 
 public class KeyExchange {
 
   private final KeyPair keyPair;
+  private final JWK publicKeyJwk;
 
   public KeyExchange() {
     try {
-      var keyPairGenerator = KeyPairGenerator.getInstance("X25519");
+      var keyPairGenerator = KeyPairGenerator.getInstance("EC");
+      keyPairGenerator.initialize(256);
       this.keyPair = keyPairGenerator.generateKeyPair();
-    } catch (GeneralSecurityException e) {
+      this.publicKeyJwk = new ECKey.Builder(Curve.P_256, (ECPublicKey) keyPair.getPublic()).build();
+    } catch (NoSuchAlgorithmException e) {
       throw new RuntimeException(e);
     }
   }
 
   public String getPublicKey() {
-    byte[] publicKey = keyPair.getPublic().getEncoded();
-    return Base64.getUrlEncoder().encodeToString(publicKey);
+    return this.publicKeyJwk.toJSONString();
   }
 
   public byte[] establishAes256bitKey(String peerPublicKey) {
     try {
-      KeyAgreement keyAgreement = KeyAgreement.getInstance("X25519");
+      KeyAgreement keyAgreement = KeyAgreement.getInstance("ECDH");
       keyAgreement.init(keyPair.getPrivate());
 
-      keyAgreement.doPhase(fromBase64(peerPublicKey), true);
+      PublicKey peerKey = JWK.parse(peerPublicKey).toECKey().toECPublicKey();
+      keyAgreement.doPhase(peerKey, true);
       byte[] secret = keyAgreement.generateSecret();
 
       return Hkdf.computeHkdf("HMACSHA256", secret, null, null, 32);
-    } catch (GeneralSecurityException e) {
+    } catch (GeneralSecurityException | ParseException | JOSEException e) {
       throw new RuntimeException(e);
     }
-  }
-
-  private PublicKey fromBase64(String peerPublicKeyInBase64Url)
-      throws NoSuchAlgorithmException, InvalidKeySpecException {
-    KeyFactory keyFactory = KeyFactory.getInstance("X25519");
-    var peerPublicKeyBytes = Base64.getUrlDecoder().decode(peerPublicKeyInBase64Url);
-    var publicKeySpec = new X509EncodedKeySpec(peerPublicKeyBytes);
-    return keyFactory.generatePublic(publicKeySpec);
   }
 }
